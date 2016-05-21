14e1bc7ec77f25a80af9aabb117dbfd865edf678
Nicky Marino <nsmarino96@gmail.com>
2016-05-20 23:07:04 -0400

    Finish home page

---
+++ application/views/pages/eboard.php
@@ -0,0 +1,1 @@
+<h1>eboard test</h1>
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -6,7 +6,7 @@
 -->
 <html>
 	<head>
-		<title>Helios by HTML5 UP</title>
+		<title>Purdue Phoenix Club</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="home_assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -31,31 +31,30 @@
 					<!-- Nav -->
 						<nav id="nav">
 							<ul>
-								<li><a href="index.php">Home</a></li>
-								<li>
-									<a href="#">Dropdown</a>
-									<ul>
-										<li><a href="#">Lorem ipsum dolor</a></li>
-										<li><a href="#">Magna phasellus</a></li>
-										<li><a href="#">Etiam dolore nisl</a></li>
-										<li>
-											<a href="#">And a submenu &hellip;</a>
-											<ul>
-												<li><a href="#">Lorem ipsum dolor</a></li>
-												<li><a href="#">Phasellus consequat</a></li>
-												<li><a href="#">Magna phasellus</a></li>
-												<li><a href="#">Etiam dolore nisl</a></li>
-											</ul>
-										</li>
-										<li><a href="#">Veroeros feugiat</a></li>
-									</ul>
-								</li>
-								<li><a href="left-sidebar.html">Left Sidebar</a></li>
-								<li><a href="right-sidebar.html">Right Sidebar</a></li>
-								<li><a href="no-sidebar.html">No Sidebar</a></li>
+								<li><a href="#">Home</a></li>
+								<li><a href="https://www.facebook.com/groups/211869608867628/?fref=phoenix_club_website">Facebook</a></li>
+								<li><a href="https://twitter.com/_phoenixclub">Twitter</a></li>
+								<li><a href="index.php/eboard">E-Board</a></li>
+								
 							</ul>
 						</nav>
 
+				</div>
+
+			<!-- Footer -->
+				<div id="footer">
+					<div class="container">
+						<div class="row">
+							<div class="12u">
+								<!-- Copyright -->
+									<div class="copyright">
+										<ul class="menu">
+											<li>&copy; <?php echo date("Y");?> <a href="https://github.com/heztet">Nicky Marino</a></li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+										</ul>
+									</div>
+							</div>
+						</div>
+					</div>
 				</div>
 
 		</div>
