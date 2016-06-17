2a4325d40a51942a46939b2c7726c4d070ca783a
Nicky Marino <nsmarino96@gmail.com>
2016-06-17 17:43:02 -0400

    Add IsCurrentYear to events table

--- dbSetup.sql
+++ dbSetup.sql
@@ -1,23 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 17, 2016 at 04:39 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.20
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `defaultdb`
+-- Database: `marinon`
 --
 
 -- --------------------------------------------------------
@@ -42,6 +24,7 @@ CREATE TABLE `phoenix_events` (
   `Title` varchar(200) NOT NULL,
   `PointValue` int(11) NOT NULL,
   `DateCreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `IsCurrentYear` tinyint(1) NOT NULL DEFAULT '1',
   `TotalStudents` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -118,7 +101,7 @@ ALTER TABLE `phoenix_current_event`
 -- AUTO_INCREMENT for table `phoenix_events`
 --
 ALTER TABLE `phoenix_events`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT for table `phoenix_records`
 --
