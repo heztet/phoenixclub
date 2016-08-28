eb5c1175e90948779775684af01967db749fda18
Nicky Marino <nsmarino96@gmail.com>
2016-08-28 16:20:51 -0400

    Remove checkin btn when closed

--- application/config/database.php
+++ application/config/database.php
@@ -73,30 +73,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-/*
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'marinon',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => 'phoenix_',
-	'pconnect' => TRUE,
-	'db_debug' => TRUE, // (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-*/
-
 // Settings for Purdue directory
 
 $db['default']['hostname'] = 'localhost';
